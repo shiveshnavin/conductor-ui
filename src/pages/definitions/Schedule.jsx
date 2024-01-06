47c33ac00bc9b48207f0840403406a279232d2eb
Shivesh Navin <shiveshnavin@gmail.com>
2024-01-06 22:56:38 +0530

    Fixe

--- src/pages/definitions/Schedule.jsx
+++ src/pages/definitions/Schedule.jsx
@@ -1,33 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, DataTable, Button } from "../../components";
 import { makeStyles } from "@material-ui/styles";
 import Header from "./Header";
 import sharedStyles from "../styles";
 import { Helmet } from "react-helmet";
 import AddIcon from "@material-ui/icons/Add";
-import { useScheduleDefs } from "../../data/schedule";
+import { useSaveSchedule, useScheduleDefs } from "../../data/schedule";
+import { useAppContext } from "../../export";
 const useStyles = makeStyles(sharedStyles);
-
-const columns = [
-  {
-    name: "wfName",
-    renderer: (name) => <NavLink path={`/scheduleDef/${name}`}>{name}</NavLink>,
-  },
-  { name: "createTime", type: "date" },
-  { name: "updateTime", type: "date" },
-  { name: "createdBy" },
-  { name: "updatedBy" },
-  { name: "ownerApp" },
-  { name: "updatedBy" },
-  { name: "wfVersion" },
-  { name: "status" },
-  { name: "cronExpression" },
-  { name: "wfInput", type: "json", sortable: false },
-];
 
 export default function ScheduleDefinitions() {
   const classes = useStyles();
   const { data: tasks, isFetching } = useScheduleDefs();
+
+  const [loading, isLoading] = useState(false)
+  const columns = [
+    {
+      name: "wfName",
+      renderer: (name) => <NavLink path={`/scheduleDef/${name}`}>{name}</NavLink>,
+    },
+    { name: "createTime", type: "date" },
+    { name: "updateTime", type: "date" },
+    { name: "createdBy" },
+    { name: "updatedBy" },
+    { name: "ownerApp" },
+    { name: "updatedBy" },
+    { name: "wfVersion" },
+    {
+      name: "status",
+      renderer: (status, body) => {
+        const { fetchWithContext } = useAppContext();
+
+        let newStatus = status;
+        if (status == 'RUN') {
+          newStatus = 'SHUTDOWN'
+        } else if (status == 'SHUTDOWN') {
+          newStatus = 'RUN'
+        }
+        return (
+          <a style={{
+            cursor: 'pointer'
+          }} class="MuiTypography-root MuiLink-root MuiLink-underlineHover MuiTypography-colorPrimary" onClick={() => {
+
+            isLoading(true)
+            const path = "/scheduling/metadata/scheduleWf";
+            fetchWithContext(path + '/' + body.wfName + "?status=" + newStatus, {
+              method: "put",
+            }).then((res) => {
+              window.location.reload()
+            }).catch(err => {
+              alert(err.message)
+            })
+
+          }}>{status}</a>
+        )
+      },
+    }
+    ,
+    { name: "cronExpression" },
+    { name: "wfInput", type: "json", sortable: false },
+  ];
 
   return (
     <div className={classes.wrapper}>
@@ -35,7 +67,7 @@ export default function ScheduleDefinitions() {
         <title>Conductor UI - Schedule Definitions</title>
       </Helmet>
 
-      <Header tabIndex={3} loading={isFetching} />
+      <Header tabIndex={3} loading={isFetching || loading} />
 
       <div className={classes.tabContent}>
         <div className={classes.buttonRow}>
